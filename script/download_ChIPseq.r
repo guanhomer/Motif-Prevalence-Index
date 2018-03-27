19ab4d37f010516deb0937caee28d3657faef46e
guanhomer <guanhomer@yahoo.com.tw>
2018-03-27 12:19:09 +0100

    Update and rename download.r to download_ChIPseq.r

--- script/download_ChIPseq.r
+++ script/download_ChIPseq.r
@@ -1,4 +1,4 @@
-dat = read.table('metadata.tsv',sep="\t",header=T,stringsAsFactors=F,check.names=F)
+dat = read.table('Downloaded_ChIPseq_Accession.tsv',sep="\t",header=T,stringsAsFactors=F,check.names=F)
 dat = dat[dat[,'Assembly']=='hg19',]
 
 unique(dat[,'Experiment target'])
@@ -9,7 +9,7 @@ dat[,'Experiment target'] = sub('^eGFP-','',dat[,'Experiment target'])
 dat[,'Experiment target'] = sub('_isoform.+','',dat[,'Experiment target'])
 
 ##read MPI
-MPI = read.table('../../cisbp/DBD_prevalence_index.csv',sep="\t",stringsAsFactors=F,header=T,comment='')
+MPI = read.table('../data/MPI.tsv',sep="\t",stringsAsFactors=F,header=T,comment='')
 MPI = MPI[MPI[,3]=='Homo_sapiens',]
 sum(toupper(unique(dat[,'Experiment target']))%in%toupper(MPI[,2])) #425
 dat = dat[toupper(dat[,'Experiment target'])%in%toupper(MPI[,2]),]
@@ -29,14 +29,10 @@ for (i in unique(dat[,'Experiment accession']))
 	for (j in dat[ind,'File download URL'])
 		{nam = sub('^.+\\/','',j)
 		if (!file.exists(paste0('Data/',nam)))
-			{system(paste0('wget -P Data ',j))
-			#print(paste0('wget -P Data ',j))
+			{system(paste0('wget -P ../data/ChIPseq ',j))
 			}
 		}
 	}
 system('sleep 60')
 }
-
-
-
 
